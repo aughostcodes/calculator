47a0b6417848d080eedfbd5b254bcc9232a7f30d
August <hello@aughost.codes>
2020-03-03 20:32:16 -0500

    Fonts

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-console.log('yo');
+'use strict';
+
+const input = [];
+
+
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function sum(args) {
+    if (args.length === 0) {
+        return 0;
+    } else {
+        let result = args.reduce((acc, val) => {
+            return acc + val;
+        })
+        return result;
+    }
+}
+
+function multiply(args) {
+    let result = args.reduce((acc, val) => {
+        return acc * val;
+    })
+    return result;
+}
+
+function power(base, expo) {
+    let total = 1;
+    for (let i = 0; i < expo; i++) {
+        total *= base;
+    }
+    return total;
+}
+
+function factorial(num) {
+    let result = 1;
+    if (num === 0) {
+        return 1;
+    } else {
+        for (let i = 1; i <= num; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
