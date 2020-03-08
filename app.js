ae3f9d7ffc7528c58f0bced6177ac6aa66f435c4
August <hello@aughost.codes>
2020-03-08 04:19:26 -0400

    Some comments

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
 'use strict';
 
-const input = [];
-const operator = [];
 const buttons = document.querySelector('.buttons');
+
+let numericalInput1 = [];
+let numericalInput2 = [];
+let num1;
+let operator = [];
+
+/*
+    Take a numerical input by user clicking on numbers,
+    store each number in an array,
+    and join the array together for one whole number.
+    If an operator is clicked,
+    any numerical input up to then is stored as the first num.
+    ie, 2 +. 345 -
+    Get second input like the first,
+    and perform the operator between num1 and num2
+    when equals is clicked.
+
+    If operator is multiply and 0 is either num1 or num2,
+    return 0.
+
+
+*/
 
 buttons.addEventListener('click', function (event) {
     console.log(event.target.textContent);
 
+    if (event.target.classList.contains('number')) {
+        numericalInput1.push(event.target.textContent);
+        console.log(numericalInput1);
+    }
+
     if (event.target.classList.contains('operator')) {
         operator.push(event.target.textContent);
-        console.log('an operator');
+        numericalInput1 = Number(numericalInput1.join(''));
+        num1 = numericalInput1;
+        console.log(numericalInput1);
+        console.log(num1);
         console.log(operator);
     }
+
+ // switch statement for math functions (add, sub, etc);
 })
 
+// Mathematical functions
 
 function add(num1, num2) {
     return num1 + num2;
